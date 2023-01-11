b9bed65c7a0225c31755d8aca37cd86415de6645
ImGili <18017158729@163.com>
2023-01-11 14:17:18 +0800

    🎉 init: first test

--- src/App/source/main.cpp
+++ src/App/source/main.cpp
@@ -1,13 +1,19 @@
-#include <iostream>
-#include <QApplication>
-#include "ExMainWindow.h"
+#include <CGAL/Simple_cartesian.h>
+#include <CGAL/Surface_mesh.h>
+#include <CGAL/draw_surface_mesh.h>
+#include <fstream>
+typedef CGAL::Simple_cartesian<double>                       Kernel;
+typedef Kernel::Point_3                                      Point;
+typedef CGAL::Surface_mesh<Point>                            Mesh;
 int main(int argc, char* argv[])
 {
-	QApplication a(argc, argv);
-
-	ExMainWindow w;
-
-	w.show();
-
-	return a.exec();
+  const std::string filename = (argc>1) ? argv[1] : CGAL::data_file_path("meshes/elephant.off");
+  Mesh sm;
+  if(!CGAL::IO::read_polygon_mesh(filename, sm))
+  {
+    std::cerr << "Invalid input file." << std::endl;
+    return EXIT_FAILURE;
+  }
+  CGAL::draw(sm);
+  return EXIT_SUCCESS;
 }

efc57b862444e7e162bb06d6d4a44a3a9eef3d13
StoneHappy <18017158729@163.com>
2023-01-11 23:55:03 +0800

    ✨ feat: pre & construct

--- src/Runtime/Series/index.h
+++ src/Runtime/Series/index.h
@@ -1,4 +1,6 @@
 #pragma once
 int a_test_line_intersect();
 
+int b_predicates_and_constructions();
+
 int miniqtproject();
--- test/mainTest.cpp
+++ test/mainTest.cpp
@@ -5,6 +5,11 @@ TEST(testCGAL, a_Line_Intersect)
 	a_test_line_intersect();
 }
 
+TEST(testCGAL, b_predicates_and_constructions)
+{
+	b_predicates_and_constructions();
+}
+
 TEST(testCGAL, miniqtproject)
 {
 	miniqtproject();
